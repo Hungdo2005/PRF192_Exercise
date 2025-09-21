16e28d6cab90d1d5de7d2a5a34a9491df02deb7d
Do Ba Quang Hung <quanghung2005thd@gmail.com>
2025-09-21 18:45:24 +0700

    Exercisel.c

---
+++ Exercise1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    double sum = 0;
+    if(n > 0){
+        for(int i = 1; i <= n; i++){
+            sum += (double) 1/i;
+        }
+        printf("%.3lf", sum);
+    }
+    return 0;
+}
